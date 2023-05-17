50f3a4b1d3b2801881a0d12f17524d6bde1783b4
Kemail-terminal <sulkemref@gmail.com>
2023-05-17 12:21:22 -0500

    Part 1 complete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,7 @@ values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'admin'
         1,'$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK');
 
 -- Abc1
+-- $2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK
 
 -- INSERT INTO roles(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, description)
 -- VALUES ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Admin'),
