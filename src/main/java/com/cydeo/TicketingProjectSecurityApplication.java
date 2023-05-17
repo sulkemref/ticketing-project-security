525a88d190dab7581c3a5ab93d53d9a657e68a43
Kemail-terminal <sulkemref@gmail.com>
2023-05-17 12:06:01 -0500

    Part 1 complete

--- src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
@@ -1,12 +1,16 @@
 package com.cydeo;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication  //this includes @Configuration
 public class TicketingProjectSecurityApplication {
+
 
     public static void main(String[] args) {
 
@@ -17,5 +21,12 @@ public class TicketingProjectSecurityApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,10 +1,7 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
