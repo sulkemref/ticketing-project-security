66b3a875872ca2787f158efe85bc0e20961ce487
Kemail-terminal <sulkemref@gmail.com>
2023-07-21 18:35:13 -0500

    Part Docker

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -44,7 +44,9 @@ public class UserDTO {
     private boolean enabled;
 
     @NotBlank
-    @Pattern(regexp = "^\\d{10}$")
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$")
     private String phone;
 
     @NotNull
